c95703e4498ba024306b5c402b2e9b9d1f765bae
Ilia Nemankov <ilia.nemankov@gmail.com>
2019-12-15 10:48:00 +0200

    Add working DTO

--- controller/working_controller.py
+++ controller/working_controller.py
@@ -3,6 +3,7 @@ import json
 from flask import request
 from flask_restplus import Namespace, Resource
 
+from dto.working_dto import working_dto
 from service.working_service import start_working, stop_working
 
 working_namespace = Namespace('api/working', description='')
@@ -10,6 +11,7 @@ working_namespace = Namespace('api/working', description='')
 
 @working_namespace.route('/start')
 class Working(Resource):
+    @working_namespace.expect(working_dto, validate=True)
     def post(self):
         start_working(data=json.loads(request.data))
         pass
@@ -17,6 +19,7 @@ class Working(Resource):
 
 @working_namespace.route('/stop')
 class Working(Resource):
+    @working_namespace.expect(working_dto, validate=True)
     def post(self):
         stop_working(data=json.loads(request.data))
         pass
